b6a1eaecebd82e806091ae3704c139a96d514bfb
TasrinSultana <tasrinsultanahappy@gmail.com>
2018-09-16 16:07:12 +0600

    add src/HelloworldRun.java

---
+++ src/HelloWorldRun.java
@@ -0,0 +1,13 @@
+
+public class HelloWorldRun {
+	public static void main(String[] args) {
+		helloWorld hw = (name) -> "Hello " + name;
+		String h = hw.sayHello("Happy");
+		System.out.println(h);
+		
+//		HelloWorld hw1 = (name) -> {
+//			
+//		}
+	}
+
+}
---
+++ src/helloWorld.java
@@ -0,0 +1,4 @@
+
+public interface helloWorld {
+	public String sayHello(String name);
+}
